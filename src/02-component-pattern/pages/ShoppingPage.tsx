3803acfd99038f745df9435c2aaabd85a50f0159
Aksel Rivera <aksel.rivera@softtek.com>
2022-02-24 09:58:20 -0600

    Component Extend Styles listo

--- src/02-component-pattern/pages/ShoppingPage.tsx
+++ src/02-component-pattern/pages/ShoppingPage.tsx
@@ -1,4 +1,5 @@
 import { ProductButtons, ProductCard, ProductImage, ProductTitle } from '../components/index';
+import '../styles/custom-styles.css';
 
 const product={
     id:'1',
@@ -9,7 +10,7 @@ const product={
 
 export const ShoppingPage = () => {
     return (
-        <div>
+        <div >
             <h1>Shopping Page</h1>
             <hr/>
 
@@ -19,17 +20,42 @@ export const ShoppingPage = () => {
                 flexWrap:'wrap'
             }}>
 
-                <ProductCard product={product}>
-                    <ProductImage  />
-                    {/* <ProductCard.Title title={'Hola'} /> */}
-                    <ProductTitle title='Hola mundo'/>
-                    <ProductButtons />
+                <ProductCard 
+                    product={product}
+                    className='bg-dark text-white'
+                >
+                    <ProductImage className='custom-image' />
+                    <ProductTitle title='Hola mundo' />
+                    <ProductButtons className='custom-buttons' />
                 </ProductCard>
 
-                <ProductCard product={product}>
-                    <ProductImage />
+                <ProductCard 
+                    product={product}
+                    className='bg-dark text-white'
+                >
+                    <ProductImage className='custom-image'  />
                     <ProductCard.Title title={'Cafecito'} />
-                    <ProductCard.Buttons />
+                    <ProductCard.Buttons className='custom-buttons' />
+                </ProductCard>
+                
+                <ProductCard 
+                    product={product}
+                    style={{
+                        backgroundColor:'#70D1F8'
+                    }}
+                >
+                    <ProductImage />
+                    <ProductCard.Title 
+                        style={{
+                            fontWeight:'bold'
+                        }}
+                    />
+                    <ProductCard.Buttons   
+                        style={{
+                            display:'flex',
+                            justifyContent:'center'
+                        }}
+                    />
                 </ProductCard>
             </div>
         </div>
